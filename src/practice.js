db55ed873a827a02846426a79cfae96118c7a2e0
EsakkiRajM <esakkiraj1150@gmail.com>
2024-03-02 12:53:05 +0530

    changes in checkbox

--- src/practice.js
+++ src/practice.js
@@ -6,7 +6,14 @@ let mobileNo = document.getElementById("mobileNo");
 let firstNameAlert = document.getElementById("firstNameAlert");
 let checkBoxValue = document.querySelectorAll(".checkBoxValue");
 let lastNameAlert = document.getElementById("lastNameAlert");
+let checkBoxValidateMsg = document.getElementById("checkBoxValidateMsg");
+let inputAddress1 = document.getElementById('inputAddress1');
+let inputAddress2 = document.getElementById('inputAddress2');
+let addressAlertMsg = document.getElementById("addressAlertMsg");
+let email = document.getElementById("email");
+let pinCode = document.getElementById("pinCode");
 
+//console.log(checkBoxValue.length);
 
 
 let firstNameValidation = function () {
@@ -15,13 +22,19 @@ let firstNameValidation = function () {
         lastName.disabled = false;
         male.disabled = false;
         female.disabled = false;
-    }  else if(firstName.value.length == 0) {
+        inputAddress1.disabled = false;
+        inputAddress2.disabled = false;
+        email.disabled = false;
+    } else if (firstName.value.length == 0) {
         firstName.classList.add("border-danger");
         firstNameAlert.innerHTML = "Please enter first name";
         firstNameAlert.style.color = "red";
         lastName.disabled = true;
         male.disabled = true;
         female.disabled = true;
+        inputAddress1.disabled = true;
+        inputAddress2.disabled = true;
+        email.disabled = true;
         // lastName.value = "";
     } else {
         lastName.disabled = true;
@@ -30,26 +43,7 @@ let firstNameValidation = function () {
         firstName.classList.remove("border-danger");
     }
 
-    
-    if (male.checked || female.checked) {
-        mobileNo.disabled = false;
-    } else {
-        mobileNo.disabled = true;
-        //mobileNo.value = "";
-    }
-
-    let enableCheckboxes = function () {
-        if (mobileNo.value.length === 10) {
-            checkBoxValue.forEach(checkbox => {
-                checkbox.disabled = false;
-            });
-        } else {
-            checkBoxValue.forEach(checkbox => {
-                checkbox.disabled = true;
-            });
-        }
-    }
-    enableCheckboxes();
+    commonValidation();
 
 
 
@@ -58,22 +52,22 @@ let firstNameValidation = function () {
 
 
 
-let lastNameValidation = function() {
-    
+let lastNameValidation = function () {
+
     if (lastName.value.length >= 1) {
         male.disabled = false;
         female.disabled = false;
         lastName.classList.remove("border-danger");
         lastNameAlert.innerHTML = "";
 
-    } else if(lastName.value.length == 0) {
+    } else if (lastName.value.length == 0) {
         lastName.classList.add("border-danger");
         lastNameAlert.innerHTML = "Please enter last name";
         lastNameAlert.style.color = "red";
         male.disabled = true;
-        female.disabled = true; 
+        female.disabled = true;
     }
-     else {
+    else {
         male.disabled = true;
         female.disabled = true;
         male.checked = false;
@@ -82,7 +76,12 @@ let lastNameValidation = function() {
         lastNameAlert.innerHTML = "";
     }
 
-    
+    commonValidation();
+
+}
+
+
+let commonValidation = function () {
     if (male.checked || female.checked) {
         mobileNo.disabled = false;
     } else {
@@ -102,22 +101,58 @@ let lastNameValidation = function() {
         }
     }
     enableCheckboxes();
+   
 
-}
+    let enableAddressBox = function () {
+        let checkedCount = document.querySelectorAll('.checkBoxValue:checked').length;
+        if (checkedCount >= 2) {
+            inputAddress1.disabled = false;
+            inputAddress2.disabled = false;
+            email.disabled = false;
+            checkBoxValidateMsg.innerHTML = "";
+        } else {
+            inputAddress1.disabled = true;
+            inputAddress2.disabled = true;
+            email.disabled = true;
+            checkBoxValidateMsg.innerHTML = "Please selct atleast two types of food";
+            checkBoxValidateMsg.style.color = "red";
+        }
+    };
 
+    enableAddressBox();
 
+    let enablePincodeBox = function () {
+        
+        if (inputAddress1.value.length >= 6) {
+            pinCode.disabled = false;
 
+        } else if(inputAddress1.value.length == 0){
+            addressAlertMsg.innerHTML = "Please enter Address";
+            addressAlertMsg.style.color = "red";
+            pinCode.disabled = true;
+        } else {
+            pinCode.disabled = true;
+        }
+    };
+
+    enablePincodeBox();
+    
+    
+    }
+//}
 
 firstName.addEventListener('input', firstNameValidation);
 lastName.addEventListener('input', lastNameValidation);
 //lastName.addEventListener('input', firstNameValidation);
 male.addEventListener('input', firstNameValidation);
 female.addEventListener('input', firstNameValidation);
-mobileNo.addEventListener('input', function() {
+mobileNo.addEventListener('input', function () {
     firstNameValidation();
     lastNameValidation(); // Trigger validation function when mobile number changes
 });
 checkBoxValue.forEach(checkbox => {
     checkbox.addEventListener('input', firstNameValidation);
 });
+
+
 
